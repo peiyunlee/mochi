02a437e93575169d51127c5322736e258a933b97
PEI-YUN LEE <may1092200258@gmail.com>
2020-04-03 00:31:23 +0800

    test grab

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+	public GameObject gParent;
+	public GameObject earGroup;
+	public GameObject grabOBJ;
+
+	private Collider2D cTop;
+	private Transform me;
+	private Vector3 tParent;
+	public bool canGrab;
+	bool isGrab;
+
+
+	void Start () {
+		me = earGroup.GetComponent<Transform>();
+		tParent = gParent.GetComponent<Transform>().position;
+		cTop = this.gameObject.GetComponent<Collider2D>();
+	}
+	
+	void FixedUpdate () {
+        if (Input.GetKeyDown (KeyCode.RightArrow)){
+            me.RotateAround(tParent,new Vector3(0,0,1),-20);
+			if(isGrab){
+				grabOBJ.transform.RotateAround(tParent,new Vector3(0,0,1),-20);
+			}
+		}
+        if (Input.GetKeyDown (KeyCode.LeftArrow)){
+            me.RotateAround(tParent,new Vector3(0,0,1),20);
+			if(isGrab){
+				grabOBJ.transform.RotateAround(tParent,new Vector3(0,0,1),20);
+			}
+		}
+		if(canGrab && Input.GetKeyDown (KeyCode.UpArrow)){
+			isGrab = true;
+			print("is");
+		}
+	}
+
+}
---
+++ Assets/Script/Test2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+
+	public GameObject rabbit1;
+	private Test test;
+	// Use this for initialization
+	void Start () {
+		test = rabbit1.GetComponent<Test>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		test.canGrab = true;
+		print("can");
+	}
+}
