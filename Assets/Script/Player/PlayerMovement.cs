758948a647bacbc8292f53ca6ec54e5657fceaf2
wendy <wendy556609@gmail.com>
2020-04-21 23:57:39 +0800

    04/21 Jump修正

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     float moveSpeed;
-    float jumpSpeed;
+    public float jumpSpeed;
 
     //Turn
     public Rigidbody2D earRb;//围绕的物体
@@ -14,11 +14,14 @@ public class PlayerMovement : MonoBehaviour
     public float force = 0;
     //Use this for initialization
     bool isTouch;
+    private bool canJump=true;
+    private bool isGround=true;
+    private int jumpCount=0;
 
     void Start()
     {
         moveSpeed = 3;
-        jumpSpeed = 100;
+        jumpSpeed = 250;
         rb = GetComponent<Rigidbody2D>();
 
         isTouch = false;
@@ -71,36 +74,65 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         Vector2 trans;
-        trans = new Vector2(Input.GetAxisRaw("Horizontal_" + this.tag) * moveSpeed, rb.velocity.y);
+        trans = new Vector2(Input.GetAxisRaw("Horizontal_"+this.tag) * moveSpeed, rb.velocity.y);
         rb.velocity = trans;
         earRb.velocity = trans;
+        Debug.Log(Input.GetAxisRaw("Horizontal_"+this.tag));
+        //Debug.Log(Input.GetAxisRaw("Vertical_"+this.tag));
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump_" + this.tag))
+        if (Input.GetButtonDown("Jump_"+this.tag)&&canJump)
         {
+            canJump=false;
             rb.AddForce(new Vector2(0, jumpSpeed));
-            earRb.AddForce(new Vector2(0, jumpSpeed));
+            //earRb.AddForce(new Vector2(0, jumpSpeed));
         }
     }
     void Turn()
     {
         if (isTouch)
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            if (Input.GetAxisRaw("Vertical_"+this.tag)>0)
             {
                 //earRb.AddForce (Vector2.right * 200,0);
-                earRb.velocity = new Vector3(Vector2.right.x * force, Vector2.right.y * force, 0);
+                earRb.velocity = new Vector3(Vector2.up.x * force, Vector2.up.y * force, 0);
             }
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (Input.GetAxisRaw("Vertical_"+this.tag)<0)
+            {
+                //earRb.AddForce (Vector2.right * 200,0);
+                earRb.velocity = new Vector3(Vector2.down.x * force, Vector2.down.y * force, 0);
+            }
+            if (Input.GetAxisRaw("Horizontal_"+this.tag)<0)
             {
                 earRb.velocity = new Vector3(Vector2.left.x * force, Vector2.left.y * force, 0);
             }
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (Input.GetAxisRaw("Horizontal_"+this.tag)>0)
             {
-                earRb.velocity = new Vector3(Vector2.up.x * force, Vector2.up.y * force, 0);
+                earRb.velocity = new Vector3(Vector2.right.x * force, Vector2.right.y * force, 0);
             }
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D floor) {
+        if(floor.gameObject.tag!="wall"){
+            canJump=true;
+            isGround=true;
+        }
+    }
+
+    // private void OnCollisionStay2D(Collision2D floor) {
+    //     if(floor.gameObject.tag=="ground"){
+    //         canJump=true;
+    //         isGround=true;
+    //     }
+    // }
+    
+    // private void OnCollisionExit2D(Collision2D floor) {
+    //     if(floor.gameObject.tag=="ground"){
+    //         canJump=false;
+    //         isGround=false;
+    //     }
+    // }
 }
