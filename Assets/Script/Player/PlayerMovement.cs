5be4baeaaf2d769caaae879fb4eeb168d7ecc489
wendy <wendy556609@gmail.com>
2020-04-23 12:11:41 +0800

    04/23 身體黏

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -9,22 +9,27 @@ public class PlayerMovement : MonoBehaviour
     public float jumpSpeed;
 
     //Turn
-    public Rigidbody2D earRb;//围绕的物体
+    public GameObject ear;
+    private Rigidbody2D earRb;//围绕的物体
+    private HingeJoint2D earJoint;
 
     public float force = 0;
     //Use this for initialization
-    bool isTouch;
-    private bool canJump=true;
-    private bool isGround=true;
-    private int jumpCount=0;
+    private bool bodyIsTouch = false;
+    private bool earIsTouch = false;
+    private bool canJump = true;
+    private int jumpCount = 0;
+    private bool canTouch = true;
+    private bool isStick = false;
+    private float touchDir = 0f;
 
     void Start()
     {
         moveSpeed = 3;
         jumpSpeed = 250;
         rb = GetComponent<Rigidbody2D>();
-
-        isTouch = false;
+        earRb = ear.GetComponent<Rigidbody2D>();
+        earJoint = ear.GetComponent<HingeJoint2D>();
 
         Check();
     }
@@ -55,84 +60,143 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X)) isTouch = !isTouch;
+        if(Input.GetButtonDown("BodyStick_" + this.tag)){
+            isStick=true;
+        }
+        else if(Input.GetButtonUp("BodyStick_" + this.tag)){
+            isStick=false;
+        }
+        
     }
 
     void FixedUpdate()
     {
-        if (!isTouch)
+        if (!bodyIsTouch && !earIsTouch)
         {
             Move();
 
             Jump();
         }
+        Touch();
 
-        Turn();
+        EarTurn();
 
+    }
+
+    void Touch()
+    {
+        if (canTouch && isStick)
+        {
+            bodyIsTouch = true;
+            
+        }
+        else if(!isStick)
+        {
+            bodyIsTouch = false;
+        }
+        if (bodyIsTouch)
+        {
+            rb.velocity=new Vector2(0,0);
+            rb.isKinematic=true;
+            earRb.isKinematic=false;
+            earJoint.enabled=true;
+            // if(touchDir>0){
+            //     JointAngleLimits2D jointAngle=earJoint.limits;
+            //     jointAngle.min=-180;
+            //     jointAngle.max=0;
+            //     earJoint.limits=jointAngle;
+            // }
+            // else if(touchDir<0){
+            //     JointAngleLimits2D jointAngle=earJoint.limits;
+            //     jointAngle.min=0;
+            //     jointAngle.max=180;
+            //     earJoint.limits=jointAngle;
+            // }
+        }
+        else{
+            earJoint.enabled=false;
+            earRb.isKinematic=true;
+            rb.isKinematic=false;
+        }
+        //Debug.Log(bodyIsTouch);
     }
 
     void Move()
     {
         Vector2 trans;
-        trans = new Vector2(Input.GetAxisRaw("Horizontal_"+this.tag) * moveSpeed, rb.velocity.y);
+        trans = new Vector2(Input.GetAxisRaw("Horizontal_" + this.tag) * moveSpeed, rb.velocity.y);
         rb.velocity = trans;
-        earRb.velocity = trans;
-        Debug.Log(Input.GetAxisRaw("Horizontal_"+this.tag));
+        //earRb.velocity = trans;
+
         //Debug.Log(Input.GetAxisRaw("Vertical_"+this.tag));
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump_"+this.tag)&&canJump)
+        if (Input.GetButtonDown("Jump_" + this.tag) && canJump)
         {
-            canJump=false;
+            canTouch=false;
+            canJump = false;
             rb.AddForce(new Vector2(0, jumpSpeed));
             //earRb.AddForce(new Vector2(0, jumpSpeed));
         }
     }
-    void Turn()
+    void EarTurn()
     {
-        if (isTouch)
+        if (bodyIsTouch)
         {
-            if (Input.GetAxisRaw("Vertical_"+this.tag)>0)
+            if (Input.GetAxisRaw("Vertical_" + this.tag) > 0)
             {
                 //earRb.AddForce (Vector2.right * 200,0);
                 earRb.velocity = new Vector3(Vector2.up.x * force, Vector2.up.y * force, 0);
             }
-            if (Input.GetAxisRaw("Vertical_"+this.tag)<0)
+            if (Input.GetAxisRaw("Vertical_" + this.tag) < 0)
             {
                 //earRb.AddForce (Vector2.right * 200,0);
                 earRb.velocity = new Vector3(Vector2.down.x * force, Vector2.down.y * force, 0);
             }
-            if (Input.GetAxisRaw("Horizontal_"+this.tag)<0)
+            if (Input.GetAxisRaw("Horizontal_" + this.tag) < 0)
             {
                 earRb.velocity = new Vector3(Vector2.left.x * force, Vector2.left.y * force, 0);
             }
-            if (Input.GetAxisRaw("Horizontal_"+this.tag)>0)
+            if (Input.GetAxisRaw("Horizontal_" + this.tag) > 0)
             {
                 earRb.velocity = new Vector3(Vector2.right.x * force, Vector2.right.y * force, 0);
             }
         }
-    }
-
-    private void OnCollisionEnter2D(Collision2D floor) {
-        if(floor.gameObject.tag!="wall"){
-            canJump=true;
-            isGround=true;
+        else{
+            earRb.velocity = new Vector3(0, 0, 0);
         }
     }
 
-    // private void OnCollisionStay2D(Collision2D floor) {
-    //     if(floor.gameObject.tag=="ground"){
-    //         canJump=true;
-    //         isGround=true;
-    //     }
-    // }
-    
-    // private void OnCollisionExit2D(Collision2D floor) {
-    //     if(floor.gameObject.tag=="ground"){
-    //         canJump=false;
-    //         isGround=false;
-    //     }
-    // }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag != "wall")
+        {
+            canJump = true;
+            
+        }
+        canTouch=true;
+        // if (other.gameObject.tag == "wall")
+        // {
+        //     canTouch = true;
+            
+        // }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        // if (other.gameObject.tag == "wall")
+        // {
+        //     canTouch = true;
+        // }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        // if (other.gameObject.tag == "wall")
+        // {
+        //     canTouch = false;
+        // }
+    }
 }
---
+++ Assets/Script/Player/PlayerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour {
+public int force;
+public Rigidbody2D earRb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Vertical_" + this.tag) > 0)
+            {
+                //earRb.AddForce (Vector2.right * 200,0);
+                earRb.velocity = new Vector3(Vector2.up.x * force, Vector2.up.y * force, 0);
+            }
+            if (Input.GetAxisRaw("Vertical_" + this.tag) < 0)
+            {
+                //earRb.AddForce (Vector2.right * 200,0);
+                earRb.velocity = new Vector3(Vector2.down.x * force, Vector2.down.y * force, 0);
+            }
+            if (Input.GetAxisRaw("Horizontal_" + this.tag) < 0)
+            {
+                earRb.velocity = new Vector3(Vector2.left.x * force, Vector2.left.y * force, 0);
+            }
+            if (Input.GetAxisRaw("Horizontal_" + this.tag) > 0)
+            {
+                earRb.velocity = new Vector3(Vector2.right.x * force, Vector2.right.y * force, 0);
+            }
+	}
+}
