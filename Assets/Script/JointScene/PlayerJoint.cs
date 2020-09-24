ba18ebe5ccbebd5ceb4b066ed37a2cd568244f7a
PEI-YUN LEE <may1092200258@gmail.com>
2020-09-24 18:16:14 +0800

    流程連接

---
+++ Assets/Script/JointScene/PlayerJoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJoint : MonoBehaviour
+{
+
+    [SerializeField]
+    bool isReady = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(isReady)
+			SceneController.Instance.LoadNextScene("Play");
+    }
+}
---
+++ Assets/Script/MainScene/UIController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+
+    public List<Transform> levelUI;
+
+    int currentLevel;
+	
+    float distance;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentLevel = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(distance);
+        Zoom();
+    }
+
+    public void UIBtnClick(string type)
+    {
+        if (type == "Right")
+        {
+            distance = -340.0f;
+            currentLevel++;
+        }
+        else
+        {
+            distance = -340.0f;
+            currentLevel--;
+        }
+    }
+
+    void Move(float distance)
+    {
+        levelUI.ForEach(delegate (Transform item)
+        {
+            float newPosX = Mathf.Lerp(item.position.x, item.position.x + distance, Time.deltaTime);
+
+            item.position = new Vector3(newPosX, item.position.y, item.position.z);
+
+        });
+
+
+    }
+
+    void Zoom()
+    {
+    }
+}
