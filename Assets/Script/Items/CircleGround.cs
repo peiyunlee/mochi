167372b4c84a36a1803df139940368ed58e7b814
PEI-YUN LEE <may1092200258@gmail.com>
2020-11-08 18:23:26 +0800

    ground

---
+++ Assets/Script/Items/CircleGround.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleGround : MonoBehaviour {
+	
+        public float rotateDeg;
+
+        void Update()
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, 0, rotateDeg));
+        }
+}
+
+        
