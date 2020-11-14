ac90ba31c2067ab3426826d70991da50388699f5
PEI-YUN LEE <may1092200258@gmail.com>
2020-11-14 19:54:35 +0800

    background move scale

---
+++ Assets/Script/Background.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	public Camera c;
+
+	float field;
+
+	float size;
+
+	public float scale;
+
+	public float offsetY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		field = c.fieldOfView;
+		
+		size = (field - 60) * scale + 1;
+
+		this.gameObject.transform.localScale = new Vector2(size,size);
+	}
+}
