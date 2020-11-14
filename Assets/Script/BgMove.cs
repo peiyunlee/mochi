daab17f176cb302b6f727c456383d78e77e2479f
PEI-YUN LEE <may1092200258@gmail.com>
2020-11-14 23:46:46 +0800

    background move script finish

---
+++ Assets/Script/BgMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgMove : MonoBehaviour
+{
+
+    public float speed;
+
+    public Vector2 turn;
+
+    public Vector2 start;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x <= turn.x)
+        {
+            transform.position = new Vector3(start.x, transform.position.y, 0f);
+            pos = transform.position;
+        }
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
