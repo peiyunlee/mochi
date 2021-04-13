baaa5bdd0578e94533db684fd566c9904c244c2e
wendy <wendy556609@gmail.com>
2021-04-13 21:41:49 +0800

    04/13
    木板修改、擺錘

---
+++ Assets/Script/Items/BreakWooden.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakWooden : MonoBehaviour
+{
+    public WoodenCollider woodenCollider;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (woodenCollider.breakCount >= 3)
+        {
+            Active(false);
+        }
+    }
+
+    void Active(bool setActive)
+    {
+        woodenCollider.gameObject.SetActive(setActive);
+    }
+}
